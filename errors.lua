ff51b659450d6b07b51f8f62c9214f5b59cc0e39
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2000-01-17 14:35:55 -0200

    teste de novas mensagens de erro (com identificador)

--- errors.lua
+++ errors.lua
@@ -7,6 +7,19 @@ function doit (s)
   return m.msg;
 end
 
+
+assert(strfind(doit"a=1; bbbb=2; bbbb(3)", "global `bbbb'"))
+assert(strfind(doit"a=1; local a,bbbb=2,3; bbbb(3)", "local `bbbb'"))
+assert(strfind(doit"a={}; a:bbbb(3)", "field `bbbb'"))
+assert(strfind(doit"local a={}; a.bbbb(3)", "field `bbbb'"))
+assert(not strfind(doit"a={13}; local bbbb=1; a[bbbb](3)", "bbbb"))
+assert(strfind(doit"a={13}; local bbbb=1; a[bbbb](3)", "number"))
+
+assert(strfind(doit"aaa.bbb:ddd(9)", "global `aaa'"))
+assert(strfind(doit"local aaa={bbb=1}; aaa.bbb:ddd(9)", "field `bbb'"))
+assert(strfind(doit"local aaa={bbb={}}; aaa.bbb:ddd(9)", "field `ddd'"))
+assert(doit"local aaa={bbb={ddd=next}}; aaa.bbb:ddd(nil)" == nil)
+
 i = 0
 function y () i=i+1; y() end
 

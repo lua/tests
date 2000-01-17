488ac3ead99ef55d58b66ec6bf99cc694094d4ac
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2000-01-17 14:35:02 -0200

    teste de expressoes/nomes em construtores

--- constructs.lua
+++ constructs.lua
@@ -6,11 +6,20 @@ function f (i)
   if i < 10 then return 'a'
   elseif i < 20 then return 'b'
   elseif i < 30 then return 'c'
-  else return 'd'
+  else return 8
   end
 end
 
-assert(f(3) == 'a' and f(12) == 'b' and f(26) == 'c' and f(100) == 'd')
+assert(f(3) == 'a' and f(12) == 'b' and f(26) == 'c' and f(100) == 8)
 
+local a, b = nil, 23
+x = {f(100)*2+3 or a, a or b+2}
+assert(x[1] == 19 and x[2] == 25)
+x = {f=2+3 or a, a = b+2}
+assert(x.f == 5 and x.a == 25)
+
+a={y=1}
+x = {a.y}
+print(x[1])
 
 print'OK'

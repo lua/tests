2b7f032ad31788bd0167bcf243ef6430c79caea5
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2001-12-19 16:11:23 -0200

    another bug in next (when n is a power of 2)

--- nextvar.lua
+++ nextvar.lua
@@ -195,15 +195,22 @@ do
   print'+'
 end
 
-a = {1,2,3,4,5;x=1,y=2,z=3}
-b = {}
-foreach(a, function (k,v) b[k] = v end)
-for k,v in b do assert(a[k] == v) end
-for k,v in a do assert(b[k] == v) end
-b = {}
-do local k,v = next(a); while k do b[k] = v; k,v = next(a,k) end end
-for k,v in b do assert(a[k] == v) end
-for k,v in a do assert(b[k] == v) end
+function checknext (a)
+  local b = {}
+  foreach(a, function (k,v) b[k] = v end)
+  for k,v in b do assert(a[k] == v) end
+  for k,v in a do assert(b[k] == v) end
+  b = {}
+  do local k,v = next(a); while k do b[k] = v; k,v = next(a,k) end end
+  for k,v in b do assert(a[k] == v) end
+  for k,v in a do assert(b[k] == v) end
+end
+
+checknext{1;x=1,y=2,z=3}
+checknext{1,2;x=1,y=2,z=3}
+checknext{1,2,3;x=1,y=2,z=3}
+checknext{1,2,3,4;x=1,y=2,z=3}
+checknext{1,2,3,4,5;x=1,y=2,z=3}
 
 assert(getn{n=20} == 20)
 assert(getn{1,2,3; n=1} == 1)

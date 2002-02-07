162afa02c086d6a82b00a5f3542193ac37991886
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2002-02-07 15:19:33 -0200

    bug: test that all trues are equal

--- api.lua
+++ api.lua
@@ -25,6 +25,10 @@ assert(T.s2d(a) == 12458954321123)
 a,b,c = T.testC("pushnum 1; pushnum 2; pushnum 3; return 2")
 assert(a == 2 and b == 3 and not c)
 
+-- test that all trues are equal
+a,b,c = T.testC("pushbool 1; pushbool 2; pushbool 0; return 3")
+assert(a == b and a == true and c == false)
+
 a,b,c = T.testC("gettop; return 2", 10, 20, 30, 40)
 assert(a == 40 and b == 5 and not c)
 

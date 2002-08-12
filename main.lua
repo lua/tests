12331b9c3ef1b29f2197a7aab8b24b29c1dc6254
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2002-08-12 13:15:51 -0300

    new options for lua.c

--- main.lua
+++ main.lua
@@ -6,7 +6,11 @@ prog = tmpname()
 otherprog = tmpname()
 out = tmpname()
 
-progname = '"'..getargs()[0]..'"'
+do
+  local i = 0
+  while arg[i] do i=i-1 end
+  progname = '"'..arg[i+1]..'"'
+end
 print(progname)
 
 local prepfile = function (s, p)
@@ -43,35 +47,33 @@ end
 -- test 2 files
 prepfile("print(1); a=2")
 prepfile("print(a)", otherprog)
-RUN("lua %s %s > %s", prog, otherprog, out)
+RUN("lua -l %s -l%s > %s", prog, otherprog, out)
 checkout("1\n2\n")
 
 local a = [[
   assert(arg.n == 3 and arg[1] == 'a' and arg[2] == 'b' and arg[3] == 'c')
-  x = getargs()
-  assert(x[1] == '-c' and x[x.n] == arg[3])
+  assert(arg[-1] == '--' and arg[-2] == "-e " and arg[-3] == %s)
 ]]
+a = format(a, progname)
 prepfile(a)
-RUN("lua -c -f %s a b c", prog)
-prepfile(a)
-RUN("lua -c -f%s a b c", prog)
+RUN('lua "-e " -- %s a b c', prog)
 
 prepfile"assert(arg==nil)"
-RUN("lua %s", prog)
+RUN("lua -l%s", prog)
 
 prepfile""
 RUN("lua - < %s > %s", prog, out)
 checkout("")
 
-RUN('lua "-eprint(1)" a=b -e "print(a)" > %s', out)
-checkout("1\nb\n")
+RUN([[lua "-eprint(1)" -ea=3 -e "print(a)" > %s]], out)
+checkout("1\n3\n")
 
 prepfile[[
   print(
 1, a
 )
 ]]
-RUN("lua a=nil - < %s > %s", prog, out)
+RUN("lua - < %s > %s", prog, out)
 checkout("1\tnil\n")
 
 prepfile[[
@@ -80,14 +82,14 @@ a
 = 10
 print(a)
 = a]]
-RUN("lua _PROMPT= _PROMPT2= -i < %s > %s", prog, out)
+RUN([[lua -e"_PROMPT='' _PROMPT2=''" -i < %s > %s]], prog, out)
 checkout("6\n10\n10\n\n")
 
 prompt = "alo"
 prepfile[[ --
 a = 2
 ]]
-RUN("lua _PROMPT=%s -i < %s > %s", prompt, prog, out)
+RUN([[lua "-e_PROMPT='%s'" -i < %s > %s]], prompt, prog, out)
 checkout(strrep(prompt, 3).."\n")
 
 s = [[ -- 
@@ -104,7 +106,7 @@ assert( a == b )
 =( f( 10 ) ) ]]
 s = gsub(s, ' ', '\n\n')
 prepfile(s)
-RUN("lua _PROMPT= _PROMPT2= -i < %s > %s", prog, out)
+RUN([[lua -e"_PROMPT='' _PROMPT2=''" -i < %s > %s]], prog, out)
 checkout("11\n\n")
   
 prepfile[[#comment in 1st line without \n at the end]]
@@ -112,7 +114,7 @@ RUN("lua %s", prog)
 
 -- close Lua with an open file
 prepfile(string.format([[io.output(%q); io.write('alo')]], out))
-RUN("lua -c %s", prog)
+RUN("lua %s", prog)
 checkout('alo')
 
 assert(remove(prog))

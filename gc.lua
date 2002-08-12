46e73657e7e86f26eaada26d4e7e551c4df4dfe5
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2002-08-12 13:14:15 -0300

    new tests + new semantics for weak tables

--- gc.lua
+++ gc.lua
@@ -7,6 +7,7 @@ setglobal("while", 234)
 limit = 5000
 
 
+
 contCreate = 0
 
 print('tabelas')
@@ -127,18 +128,31 @@ for i=1,lim do assert(a[i] == i) end
 
 
 print('weak tables')
-a = setmetatable({}, {__weakmode='k'})
+a = setmode({}, 'k'); assert(getmode(a) == 'k')
 -- fill a with some `collectable' indices
 for i=1,lim do a[{}] = i end
 -- and some non-collectable ones
 for i=1,lim do local t={}; a[t]=t end
 for i=1,lim do a[i] = i end
+for i=1,lim do local s=strrep('@', i); a[s] = s..'#' end
 collectgarbage()
 local i = 0
-for k,v in a do assert(k==v); i=i+1 end
-assert(i == 2*lim)
+for k,v in a do assert(k==v or k..'#'==v); i=i+1 end
+assert(i == 3*lim)
 
-a = setmetatable({}, {__weakmode='v'})
+a = setmode({}, 'v'); assert(getmode(a) == 'v')
+a[1] = strrep('b', 21)
+collectgarbage()
+assert(a[1])   -- strings are *values*
+a[1] = nil
+-- test setn
+do
+  local t = {}; table.setn(t, 10);
+  assert(table.getn(t) == 10)
+  a[1] = t
+end
+collectgarbage()
+assert(a[1] == nil)
 -- fill a with some `collectable' values (in both parts of the table)
 for i=1,lim do a[i] = {} end
 for i=1,lim do a[i..'x'] = {} end
@@ -150,10 +164,11 @@ local i = 0
 for k,v in a do assert(k==v or k-lim..'x' == v); i=i+1 end
 assert(i == 2*lim)
 
-a = setmetatable({}, {__weakmode='vk'})
+a = setmode({}, 'vk'); assert(getmode(a) == 'kv')
 local x, y, z = {}, {}, {}
 -- keep only some items
 a[1], a[2], a[3] = x, y, z
+a[strrep('$', 11)] = strrep('$', 11)
 -- fill a with some `collectable' values
 for i=4,lim do a[i] = {} end
 for i=1,lim do a[{}] = i end
@@ -164,13 +179,52 @@ local i = 0
 for k,v in a do
   assert((k == 1 and v == x) or
          (k == 2 and v == y) or
-         (k == 3 and v == z));
+         (k == 3 and v == z) or k==v);
   i = i+1
 end
-assert(i == 3)
+assert(i == 4)
 x,y,z=nil
 collectgarbage()
-assert(next(a) == nil)
+assert(next(a) == strrep('$', 11))
+assert(getmode(a) == 'kv'); setmode(a, ''); assert(getmode(a) == '')
+
+
+-- teste de userdata
+collectgarbage(2^30)   -- stop collection
+local u = newproxy(true)
+local s = 0
+local a = setmode({[u] = 0}, 'vk')
+for i=1,10 do a[newproxy(u)] = i end
+for k in pairs(a) do assert(getmetatable(k) == getmetatable(u)) end
+local a1 = {}; for k,v in pairs(a) do a1[k] = v end
+for k,v in pairs(a1) do a[v] = k end
+for i =1,10 do assert(a[i]) end
+getmetatable(u).a = a1
+getmetatable(u).u = u
+do
+  local u = u
+  getmetatable(u).__gc = function (o)
+    assert(a[o] == 10-s)
+    assert(a[10-s] == nil) -- udata already removed from weak table
+    assert(getmetatable(o) == getmetatable(u))
+    assert(getmetatable(o).a[o] == 10-s)
+    s=s+1
+  end
+end
+a1, u = nil
+assert(next(a) ~= nil)
+collectgarbage(0)
+assert(next(a) ~= nil and s==11)
+collectgarbage(0)
+assert(next(a) == nil)  -- finalized keys are removed in two cycles
+
+
+
+-- erro na coleta
+u = newproxy(true)
+getmetatable(u).__gc = function () error "!!!" end
+u = nil
+assert(not pcall(collectgarbage, 0))
 
 
 if not _soft then
@@ -180,6 +234,26 @@ if not _soft then
     a = {next = a}
   end
   collectgarbage()
+end
+
+
+
+-- cria udata para ser coletado quando fechar o estado
+
+do
+  local newproxy,assert,type,print,getmetatable =
+        newproxy,assert,type,print,getmetatable
+  local u = newproxy(true)
+  local tt = getmetatable(u)
+  ___Glob = u   -- evita que udata seja coletado antes da coleta final
+  tt.__gc = function (o)
+    assert(getmetatable(o) == tt)
+    -- cria objetos durante coleta de lixo
+    local a = 'xuxu'..(10+3)..'joao', {}
+    ___Glob = o  -- ressucita objeto!
+    newproxy(o)  -- cria outro com mesma metatable
+    print(">>> fechando estado " .. "<<<\n")
+  end
 end
 
 print('OK')

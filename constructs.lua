b75d74a949d04e15450d452eef5090c172e76210
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2002-08-12 11:49:36 -0300

    new priority for and x or

--- constructs.lua
+++ constructs.lua
@@ -11,6 +11,9 @@ assert(-2^2 == -4 and (-2)^2 == 4 and 2*2-3-1 == 0);
 assert(2*1+3/3 == 3 and 1+2 .. 3*1 == "33");
 assert(not(2+1 > 3*1) and "a".."b" > "a");
 
+assert(not ((true or false) and nil))
+assert(      true or false  and nil)
+
 local a,b = 1,nil;
 assert(-(1 or 2) == -1 and (1 and 2)+(-1.25 or -4) == 0.75);
 x = ((b or a)+1 == 2 and (10 or a)+1 == 11); assert(x);
@@ -154,7 +157,7 @@ f,a = loadstring(f)();
 assert(a.a == 1 and a.b)
 
 function g (a,b,c,d,e)
-  if not (a>=b or c or (d and e) or nil) then return 0; else return 1; end;
+  if not (a>=b or c or d and e or nil) then return 0; else return 1; end;
 end
 
 function h (a,b,c,d,e)

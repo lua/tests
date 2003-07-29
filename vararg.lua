885011fd8a6b8dddcd153d1e12d71e71b60c536d
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2003-07-29 15:51:41 -0300

    syntax `...=var' changed to `var=...'

--- vararg.lua
+++ vararg.lua
@@ -9,7 +9,7 @@ function f(a, ...)
   return arg.n
 end
 
-function c12 (...=x)
+function c12 (x=...)
   local res = (x.n==2 and x[1] == 1 and x[2] == 2)
   assert(arg == nil)
   if res then res = 55 end
@@ -49,7 +49,7 @@ lim = 20
 local i, a = 1, {}
 while i <= lim do a[i] = i+0.3; i=i+1 end
 
-function f(a, b, c, d, ... = more)
+function f(a, b, c, d, more = ...)
   assert(a == 1.3 and more[1] == 5.3 and
          more[lim-4] == lim+0.3 and not more[lim-3])
 end

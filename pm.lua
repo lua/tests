53d0e185da967550e60e660115e132207bba3237
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2005-07-31 13:48:08 -0300

    "%0" stands for the whole pattern (in gsub)

--- pm.lua
+++ pm.lua
@@ -128,10 +128,11 @@ assert(string.gsub('alo  alo  \n 123\n ', '%s+', ' ') == 'alo alo 123 ')
 t = "abç d"
 a, b = string.gsub(t, '(.)', '%1@')
 assert('@'..a == string.gsub(t, '', '@') and b == 5)
-a, b = string.gsub('abçd', '(.)', '%1@', 2)
+a, b = string.gsub('abçd', '(.)', '%0@', 2)
 assert(a == 'a@b@çd' and b == 2)
 assert(string.gsub('alo alo', '()[al]', '%1') == '12o 56o')
-assert(string.gsub("abc=xyz", "(%w*)(%p)(%w+)", "%3%2%1") == "xyz=abc")
+assert(string.gsub("abc=xyz", "(%w*)(%p)(%w+)", "%3%2%1-%0") ==
+              "xyz=abc-abc=xyz")
 assert(string.gsub('áéí', '$', '\0óú') == 'áéí\0óú')
 assert(string.gsub('', '^', 'r') == 'r')
 assert(string.gsub('', '$', 'r') == 'r')

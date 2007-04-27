60da5c68199bc227f345ca2427190dccacf8c47e
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2007-04-27 16:03:23 -0300

    test for recent bug (concat metamethod x numbers)

--- events.lua
+++ events.lua
@@ -253,6 +253,17 @@ x = 0 .."a".."b"..c..d.."e".."f".."g"
 assert(x.val == "0abcdefg")
 
 
+-- concat metamethod x numbers (bug in 5.1.1)
+c = {}
+local x
+setmetatable(c, {__concat = function (a,b)
+  assert(type(a) == "number" and b == c or type(b) == "number" and a == c)
+  return c
+end})
+assert(c..5 == c and 5 .. c == c)
+assert(4 .. c .. 5 == c and 4 .. 5 .. 6 .. 7 .. c == c)
+
+
 -- test comparison compatibilities
 local t1, t2, c, d
 t1 = {};  c = {}; setmetatable(c, t1)

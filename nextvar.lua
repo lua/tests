8f7edf792dfd7464b81447f8fad334e94dd11865
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2007-10-17 15:24:59 -0200

    tests for __ipairs metamethod

--- nextvar.lua
+++ nextvar.lua
@@ -395,18 +395,42 @@ assert(x == 5)
 
 -- testing __pairs metamethod
 a = {}
+do
+  local x,y,z = pairs(a)
+  assert(type(x) == 'function' and y == a and z == nil)
+  local x,y,z = ipairs(a)
+  assert(type(x) == 'function' and y == a and z == 0)
+end
+
 local function foo (e,i)
   assert(e == a)
   if i <= 10 then return i+1, i+2 end
 end
 
-setmetatable(a, {__pairs = function (x) return foo, x, 0 end})
+local function foo1 (e,i)
+  i = i + 1
+  assert(e == a)
+  if i <= e.n then return i,a[i] end
+end
+
+setmetatable(a, {__pairs = function (x) return foo, x, 0 end,
+                 __ipairs = function (x) return foo1, x, 0 end})
 
 local i = 0
 for k,v in pairs(a) do
   i = i + 1
   assert(k == i and v == k+1)
 end
+
+a.n = 5
+a[3] = 30
+
+local i = 0
+for k,v in ipairs(a) do
+  i = i + 1
+  assert(k == i and v == a[i])
+end
+assert(i == a.n)
 
 a = {n=10}
 setmetatable(a, {__len = function (x) return x.n end,

da0d3068b748bf9908a21e4e513ab00b0eef0a92
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2009-07-01 18:24:16 -0300

    BUG: 'luaV_settable' may invalidate a reference to a table and try to reuse it..

--- events.lua
+++ events.lua
@@ -359,6 +359,18 @@ assert(getmetatable(nil) == nil)
 debug.setmetatable(nil, {})
 
 
+-- bug in 5.1.4
+grandparent = {}
+grandparent.__newindex = function(s,_,_) print(s) end
+
+parent = {}
+parent.__newindex = parent
+setmetatable(parent, grandparent)
+
+child = setmetatable({}, parent)
+child.foo = 10      --> (crash on some machines)
+
+
 print 'OK'
 
 return 12

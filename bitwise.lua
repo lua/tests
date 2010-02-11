a17e24b4868321163c54c0fff7129b02facabe9a
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2010-02-11 13:56:24 -0200

    use of 'conventional' names for shift and rotate operations + right/left versions for them

--- bitwise.lua
+++ bitwise.lua
@@ -8,20 +8,24 @@ assert(bit.bxor() == 0)
 assert(bit.band() == bit.band(0xffffffff))
 assert(bit.band(1,2) == 0)
 
-assert(bit.brotate(0, -1) == 0)
-assert(bit.brotate(0, 7) == 0)
-assert(bit.brotate(0x12345678, 4) == 0x23456781)
-assert(bit.brotate(0x12345678, -8) == 0x78123456)
-assert(bit.brotate(0xaaaaaaaa, 2) == 0xaaaaaaaa)
-assert(bit.brotate(0xaaaaaaaa, -2) == 0xaaaaaaaa)
+assert(bit.rol(0, -1) == 0)
+assert(bit.rol(0, 7) == 0)
+assert(bit.rol(0x12345678, 4) == 0x23456781)
+assert(bit.ror(0x12345678, -4) == 0x23456781)
+assert(bit.rol(0x12345678, -8) == 0x78123456)
+assert(bit.ror(0x12345678, 8) == 0x78123456)
+assert(bit.rol(0xaaaaaaaa, 2) == 0xaaaaaaaa)
+assert(bit.rol(0xaaaaaaaa, -2) == 0xaaaaaaaa)
 for i = -50, 50 do
-  assert(bit.brotate(0x89abcdef, i) == bit.brotate(0x89abcdef, i%32))
+  assert(bit.rol(0x89abcdef, i) == bit.rol(0x89abcdef, i%32))
 end
 
-assert(bit.bshift(0x12345678, 4) == 0x23456780)
-assert(bit.bshift(0x12345678, 8) == 0x34567800)
-assert(bit.bshift(0x12345678, -4) == 0x01234567)
-assert(bit.bshift(0x12345678, -8) == 0x00123456)
+assert(bit.lshift(0x12345678, 4) == 0x23456780)
+assert(bit.lshift(0x12345678, 8) == 0x34567800)
+assert(bit.lshift(0x12345678, -4) == 0x01234567)
+assert(bit.lshift(0x12345678, -8) == 0x00123456)
+assert(bit.rshift(0x12345678, 4) == 0x01234567)
+assert(bit.rshift(0x12345678, 8) == 0x00123456)
 
 print("+")
 -- some special cases
@@ -45,10 +49,12 @@ for _, b in pairs(c) do
   assert(bit.bnot(b) ~= b)
   assert(bit.bnot(bit.bnot(b)) == b)
   assert(bit.bnot(b) == 2^32 - 1 - b)
-  assert(bit.brotate(b, 32) == b)
-  assert(bit.bshift(bit.bshift(b, -4), 4) == bit.band(b, bit.bnot(0xf)))
+  assert(bit.rol(b, 32) == b)
+  assert(bit.ror(b, 32) == b)
+  assert(bit.lshift(bit.lshift(b, -4), 4) == bit.band(b, bit.bnot(0xf)))
+  assert(bit.rshift(bit.rshift(b, 4), -4) == bit.band(b, bit.bnot(0xf)))
   for i = -40, 40 do
-    assert(bit.bshift(b, i) == math.floor((b * 2^i) % 2^32))
+    assert(bit.lshift(b, i) == math.floor((b * 2^i) % 2^32))
   end
 end
 
@@ -56,7 +62,8 @@ print("+")
 
 assert(not pcall(bit.band, {}))
 assert(not pcall(bit.bnot, "a"))
-assert(not pcall(bit.bshift, 45))
-assert(not pcall(bit.bshift, 45, print))
+assert(not pcall(bit.lshift, 45))
+assert(not pcall(bit.lshift, 45, print))
+assert(not pcall(bit.rshift, 45, print))
 
 print'OK'

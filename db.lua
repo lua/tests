bdf3e38f83a0afaf285cb50920bb33c01273e6ac
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2010-11-30 15:18:32 -0200

    getlocal/setlocal can access vararg parameters

--- db.lua
+++ db.lua
@@ -182,6 +182,37 @@ assert(debug.getlocal(co, foo, 3) == nil)
 assert(debug.getlocal(print, 1) == nil)
 
 
+-- varargs
+local function foo (a, ...)
+  local t = table.pack(...)
+  for i = 1, t.n do
+    local n, v = debug.getlocal(1, -i)
+    assert(n == "(*vararg)" and v == t[i])
+  end
+  assert(not debug.getlocal(1, -(t.n + 1)))
+  assert(not debug.setlocal(1, -(t.n + 1), 30))
+  if t.n > 0 then
+    (function (x)
+      assert(debug.setlocal(2, -1, x) == "(*vararg)")
+      assert(debug.setlocal(2, -t.n, x) == "(*vararg)")
+     end)(430)
+     assert(... == 430)
+  end
+end
+
+foo()
+foo(print)
+foo(200, 3, 4)
+local a = {}
+for i = 1,1000 do a[i] = i end
+foo(table.unpack(a))
+a = nil
+
+-- access to vararg in non-vararg function
+local function foo () return debug.getlocal(1, -1) end
+assert(foo(10) == nil)
+
+
 a = {}; L = nil
 local glob = 1
 local oldglob = glob

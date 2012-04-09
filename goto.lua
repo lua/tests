b176e90f5b82bc3dac5543c217cd310b0d731701
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2012-04-09 17:15:44 -0300

    extra tests for labels with long names

--- goto.lua
+++ goto.lua
@@ -44,6 +44,24 @@ do
 end
 ::l3:: ::l3_1:: assert(x == 13)
 
+
+-- long labels
+do
+  local prog = [[
+  do
+    local a = 1
+    goto l%sa; a = a + 1
+   ::l%sa:: a = a + 10
+    goto l%sb; a = a + 2
+   ::l%sb:: a = a + 20
+    return a
+  end
+  ]]
+  local label = string.rep("0123456789", 40)
+  prog = string.format(prog, label, label, label, label)
+  assert(assert(load(prog))() == 31)
+end
+
 -- goto to correct label when nested
 do goto l3; ::l3:: end   -- does not loop jumping to previous label 'l3'
 

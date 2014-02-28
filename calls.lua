a9106a159742c20b992dbb9b37a85801a7a53a17
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2014-02-28 09:26:29 -0300

    test with a main chunk with many upvalues

--- calls.lua
+++ calls.lua
@@ -303,14 +303,28 @@ x("set")
 assert(x() == 24)
 
 -- test for dump/undump with many upvalues
-local prog = "local "
-for i = 1, 190 do prog = prog .. "a" .. i .. ", " end
-prog = prog .. "b = "
-for i = 1, 190 do prog = prog .. i .. ", " end
-prog = prog .. "0;"
-prog = prog .. "return function () return a187 end"
-local f = assert(load(prog))()
-assert(f() == 187)
+do
+  local nup = 200    -- maximum number of local variables
+  local prog = {"local a1"}
+  for i = 2, nup do prog[#prog + 1] = ", a" .. i end
+  prog[#prog + 1] = " = 1"
+  for i = 2, nup do prog[#prog + 1] = ", " .. i end
+  local sum = 1
+  prog[#prog + 1] = "; return function () return a1"
+  for i = 2, nup do prog[#prog + 1] = " + a" .. i; sum = sum + i end
+  prog[#prog + 1] = " end"
+  prog = table.concat(prog)
+  local f = assert(load(prog))()
+  assert(f() == sum)
+
+  f = load(string.dump(f))   -- main chunk now has many upvalues
+  local a = 10
+  local h = function () return a end
+  for i = 1, nup do
+    debug.upvaluejoin(f, i, h, 1)
+  end
+  assert(f() == 10 * nup)
+end
 
 -- test for bug in parameter adjustment
 assert((function () return nil end)(4) == nil)

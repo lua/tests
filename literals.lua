be1118a29efa08f837457632306883f03ac848c0
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2014-03-11 10:44:30 -0300

    removed test (not portable and already done in 'strings.lua')

--- literals.lua
+++ literals.lua
@@ -259,7 +259,6 @@ end
 
 -- testing decimal point locale
 if os.setlocale("pt_BR") or os.setlocale("ptb") then
-  assert(not load("\xE1 = 3"))  -- parser still works with C locale
   assert(not load("a = (3,4)"))
   assert(tonumber("3,4") == 3.4 and tonumber"3.4" == nil)
   assert(assert(load("return 3.4"))() == 3.4)

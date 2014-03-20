1904ceab80602871baa51449af4af7435f59a6a4
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2014-03-20 16:40:34 -0300

    added a complete implementation of bit32 library in pure Lua + a few other tests

--- bitwise.lua
+++ bitwise.lua
@@ -7,10 +7,10 @@ assert(~0 == -1)
 -- use variables to avoid constant folding
 local a, b, c, d
 a = 0xFFFFFFFFFFFFFFFF
-assert(a & -1 == a)
+assert(a == -1 and a & -1 == a and a & 35 == 35)
 a = 0xF0F0F0F0F0F0F0F0
 assert(a | -1 == -1)
-assert(a ~ a == 0)
+assert(a ~ a == 0 and a ~ 0 == a and a ~ ~a == -1)
 assert(a >> 4 == ~a)
 a = 0xF0; b = 0xCC; c = 0xAA; d = 0xFD
 assert(a | b ~ c & d == 0xF4)
@@ -18,16 +18,16 @@ assert(a | b ~ c & d == 0xF4)
 a = 0xF0.3; b = 0xCC.23; c = 0xAA.1; d = 0xFD.4
 assert(a | b ~ c & d == 0xF4)
 
-if numbits >= 64 then
-  a = 0xF000000000000000; b = 0xCC00000000000000;
-  c = 0xAA00000000000000; d = 0xFD00000000000000
-  assert(a | b ~ c & d == 0xF400000000000000)
-else
-  a = 0xF0000000; b = 0xCC000000;
-  c = 0xAA000000; d = 0xFD000000
-  assert(a | b ~ c & d == 0xF4000000)
-end
-assert(a | b < 0)
+a = 0xF0000000; b = 0xCC000000;
+c = 0xAA000000; d = 0xFD000000
+assert(a | b ~ c & d == 0xF4000000)
+assert(~~a == a and ~a == -1 ~ a and -d == ~d + 1)
+
+a = a << 32
+b = b << 32
+c = c << 32
+d = d << 32
+assert(a | b ~ c & d == 0xF4000000 << 32)
 assert(~~a == a and ~a == -1 ~ a and -d == ~d + 1)
 
 assert(-1 >> 1 == 2^(numbits - 1) - 1 and 1 << 31 == 0x80000000)
@@ -37,6 +37,9 @@ assert(-1 >> numbits == 0 and
        -1 << numbits == 0 and
        -1 << -numbits == 0)
 
+assert((2^30 - 1) << 2^30 == 0)
+assert((2^30 - 1) >> 2^30 == 0)
+
 for _, i in ipairs{0, 1, 2, 3, 4, 31, 32, 33,
                    numbits - 1, numbits, numbits + 1, numbits * 2} do
   assert(1 << i == 2^i and -1 << i == -2^i)
@@ -44,7 +47,120 @@ end
 
 assert(1 >> -3 == 1 << 3 and 1000 >> 5 == 1000 << -5)
 
+print'+'
+
+
+package.preload.bit32 = function ()     --{
+
+-- no built-in 'bit32' library: implement it using bitwise operators
+
+local bit = {}
+
+function bit.bnot (a)
+  return ~a & 0xFFFFFFFF
+end
+
+
+--
+-- in all vararg functions, avoid creating 'arg' table when there are
+-- only 2 (or less) parameters, as 2 parameters is the common case
+--
+
+function bit.band (x, y, z, ...)
+  if not z then
+    return ((x or -1) & (y or -1)) & 0xFFFFFFFF
+  else
+    local arg = {...}
+    local res = x & y & z
+    for i = 1, #arg do res = res & arg[i] end
+    return res & 0xFFFFFFFF
+  end
+end
+
+function bit.bor (x, y, z, ...)
+  if not z then
+    return ((x or 0) | (y or 0)) & 0xFFFFFFFF
+  else
+    local arg = {...}
+    local res = x | y | z
+    for i = 1, #arg do res = res | arg[i] end
+    return res & 0xFFFFFFFF
+  end
+end
+
+function bit.bxor (x, y, z, ...)
+  if not z then
+    return ((x or 0) ~ (y or 0)) & 0xFFFFFFFF
+  else
+    local arg = {...}
+    local res = x ~ y ~ z
+    for i = 1, #arg do res = res ~ arg[i] end
+    return res & 0xFFFFFFFF
+  end
+end
+
+function bit.btest (...)
+  return bit.band(...) ~= 0
+end
+
+function bit.lshift (a, b)
+  return ((a & 0xFFFFFFFF) << b) & 0xFFFFFFFF
+end
+
+function bit.rshift (a, b)
+  return ((a & 0xFFFFFFFF) >> b) & 0xFFFFFFFF
+end
+
+function bit.arshift (a, b)
+  a = a & 0xFFFFFFFF
+  if b <= 0 or (a & 0x80000000) == 0 then
+    return (a >> b) & 0xFFFFFFFF
+  else
+    return ((a >> b) | ~(0xFFFFFFFF >> b)) & 0xFFFFFFFF
+  end
+end
+
+function bit.lrotate (a ,b)
+  b = b & 31
+  a = a & 0xFFFFFFFF
+  if b ~= 0 then
+    a = (a << b) | (a >> (32 - b))
+  end
+  return a & 0xFFFFFFFF
+end
+
+function bit.rrotate (a, b)
+  return bit.lrotate(a, -b)
+end
+
+local function checkfield (f, w)
+  w = w or 1
+  assert(f >= 0, "field cannot be negative")
+  assert(w > 0, "width must be positive")
+  assert(f + w <= 32, "trying to access non-existent bits")
+  return f, ~(-1 << w)
+end
+
+function bit.extract (a, f, w)
+  local f, mask = checkfield(f, w)
+  return (a >> f) & mask
+end
+
+function bit.replace (a, v, f, w)
+  local f, mask = checkfield(f, w)
+  v = v & mask
+  a = (a & ~(mask << f)) | (v << f)
+  return a & 0xFFFFFFFF
+end
+
+return bit
+
+end  --}
+
+
 print("testing bitwise library")
+
+local bit32 = require'bit32'
 
 assert(bit32.band() == bit32.bnot(0))
 assert(bit32.btest() == true)
@@ -115,16 +231,21 @@ local c = {0, 1, 2, 3, 10, 0x80000000, 0xaaaaaaaa, 0x55555555,
 for _, b in pairs(c) do
   assert(bit32.band(b) == b)
   assert(bit32.band(b, b) == b)
+  assert(bit32.band(b, b, b, b) == b)
   assert(bit32.btest(b, b) == (b ~= 0))
   assert(bit32.band(b, b, b) == b)
+  assert(bit32.band(b, b, b, ~b) == 0)
   assert(bit32.btest(b, b, b) == (b ~= 0))
   assert(bit32.band(b, bit32.bnot(b)) == 0)
   assert(bit32.bor(b, bit32.bnot(b)) == bit32.bnot(0))
   assert(bit32.bor(b) == b)
   assert(bit32.bor(b, b) == b)
   assert(bit32.bor(b, b, b) == b)
+  assert(bit32.bor(b, b, 0, ~b) == 0xffffffff)
   assert(bit32.bxor(b) == b)
   assert(bit32.bxor(b, b) == 0)
+  assert(bit32.bxor(b, b, b) == b)
+  assert(bit32.bxor(b, b, b, b) == 0)
   assert(bit32.bxor(b, 0) == b)
   assert(bit32.bnot(b) ~= b)
   assert(bit32.bnot(bit32.bnot(b)) == b)
@@ -180,12 +301,14 @@ assert(bit32.replace(-1, 0, 1, 2) == 2^32 - 7)
 
 assert(bit32.bor(3.9) == 3)
 assert(bit32.bor(-3.9) == 0xfffffffc)
-if 2.0^50 < 2.0^50 + 1.0 then   -- large floats?
+
+-- large floats and large-enough integers?
+if 2.0^50 < 2.0^50 + 1.0 and 2.0^50 < (-1 >> 1) then
   assert(bit32.bor(2.0^32 - 4.9) == 0xfffffffb)
   assert(bit32.bor(-2.0^32 - 5.8) == 0xfffffffa)
   assert(bit32.bor(2.0^48 - 4.5) == 0xfffffffb)
   assert(bit32.bor(-2.0^48 - 5.5) == 0xfffffffa)
 end
 
-
 print'OK'
+

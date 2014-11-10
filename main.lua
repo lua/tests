97f0095826baad5e5f07768baf1ca82fb5754e53
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2014-11-10 16:48:29 -0200

    'lua' changed for '@lua' in commands + increasing test coverage

--- main.lua
+++ main.lua
@@ -49,7 +49,7 @@ end
 
 function auxrun (...)
   local s = string.format(...)
-  s = string.gsub(s, "lua", '"'..progname..'"', 1)
+  s = string.gsub(s, "@lua", '"'..progname..'"', 1)
   return os.execute(s)
 end
 
@@ -66,34 +66,43 @@ function NoRunMsg (...)
   return NoRun(...)
 end
 
+-- running stdin as a file
+RUN("@lua > %s << eof\nprint(10)\nprint(2)\neof\n", out)
+checkout("10\n2\n")
+
 -- test environment variables used by Lua
 prepfile("print(package.path)")
 
-RUN("env LUA_INIT= LUA_PATH=x lua %s > %s", prog, out)
+RUN("env LUA_INIT= LUA_PATH=x @lua %s > %s", prog, out)
 checkout("x\n")
 
-RUN("env LUA_INIT= LUA_PATH_5_3=y LUA_PATH=x lua %s > %s", prog, out)
+RUN("env LUA_INIT= LUA_PATH_5_3=y LUA_PATH=x @lua %s > %s", prog, out)
 checkout("y\n")
 
 prepfile("print(package.cpath)")
 
-RUN("env LUA_INIT= LUA_CPATH=xuxu lua %s > %s", prog, out)
+RUN("env LUA_INIT= LUA_CPATH=xuxu @lua %s > %s", prog, out)
 checkout("xuxu\n")
 
-RUN("env LUA_INIT= LUA_CPATH_5_3=yacc LUA_CPATH=x lua %s > %s", prog, out)
+RUN("env LUA_INIT= LUA_CPATH_5_3=yacc LUA_CPATH=x @lua %s > %s", prog, out)
 checkout("yacc\n")
 
 prepfile("print(X)")
-RUN('env LUA_INIT="X=tonumber(arg[1])" lua %s 3.2 > %s', prog, out)
+RUN('env LUA_INIT="X=tonumber(arg[1])" @lua %s 3.2 > %s', prog, out)
 checkout("3.2\n")
 
 prepfile("print(X)")
-RUN('env LUA_INIT_5_3="X=10" LUA_INIT="X=3" lua %s > %s', prog, out)
+RUN('env LUA_INIT_5_3="X=10" LUA_INIT="X=3" @lua %s > %s', prog, out)
 checkout("10\n")
+
+prepfile("x = x or 10; print(x); x = x + 1")
+print(prog)
+RUN("env LUA_INIT='@%s' @lua %s > %s", prog, prog, out)
+checkout("10\n11\n")
 
 -- test option '-E'
 prepfile("print(package.path, package.cpath)")
-RUN('env LUA_INIT="error(10)" LUA_PATH=xxx LUA_CPATH=xxx lua -E %s > %s',
+RUN('env LUA_INIT="error(10)" LUA_PATH=xxx LUA_CPATH=xxx @lua -E %s > %s',
      prog, out)
 local defaultpath = getoutput()
 defaultpath = string.match(defaultpath, "^(.-)\t")   -- remove tab
@@ -103,7 +112,7 @@ assert(not string.find(defaultpath, "xxx") and string.find(defaultpath, "lua"))
 -- test replacement of ';;' to default path
 local function convert (p)
   prepfile("print(package.path)")
-  RUN('env LUA_PATH="%s" lua %s > %s', p, prog, out)
+  RUN('env LUA_PATH="%s" @lua %s > %s', p, prog, out)
   local expected = getoutput()
   expected = string.sub(expected, 1, -2)   -- cut final end of line
   assert(string.gsub(p, ";;", ";"..defaultpath..";") == expected)
@@ -120,7 +129,7 @@ convert(";;a;;;bc")
 -- test 2 files
 prepfile("print(1); a=2; return {x=15}")
 prepfile(("print(a); print(_G['%s'].x)"):format(prog), otherprog)
-RUN('env LUA_PATH="?;;" lua -l %s -l%s -lstring -l io %s > %s', prog, otherprog, otherprog, out)
+RUN('env LUA_PATH="?;;" @lua -l %s -l%s -lstring -l io %s > %s', prog, otherprog, otherprog, out)
 checkout("1\n2\n15\n2\n15\n")
 
 local a = [[
@@ -133,22 +142,22 @@ local a = [[
 ]]
 a = string.format(a, progname)
 prepfile(a)
-RUN('lua "-e " -- %s a b c', prog)
+RUN('@lua "-e " -- %s a b c', prog)
 
 prepfile"assert(arg)"
 prepfile("assert(arg)", otherprog)
-RUN('env LUA_PATH="?;;" lua -l%s - < %s', prog, otherprog)
+RUN('env LUA_PATH="?;;" @lua -l%s - < %s', prog, otherprog)
 
 prepfile""
-RUN("lua - < %s > %s", prog, out)
+RUN("@lua - < %s > %s", prog, out)
 checkout("")
 
 -- test many arguments
 prepfile[[print(({...})[30])]]
-RUN("lua %s %s > %s", prog, string.rep(" a", 30), out)
+RUN("@lua %s %s > %s", prog, string.rep(" a", 30), out)
 checkout("a\n")
 
-RUN([[lua "-eprint(1)" -ea=3 -e "print(a)" > %s]], out)
+RUN([[@lua "-eprint(1)" -ea=3 -e "print(a)" > %s]], out)
 checkout("1\n3\n")
 
 prepfile[[
@@ -156,7 +165,7 @@ prepfile[[
 1, a
 )
 ]]
-RUN("lua - < %s > %s", prog, out)
+RUN("@lua - < %s > %s", prog, out)
 checkout("1\tnil\n")
 
 prepfile[[
@@ -165,19 +174,19 @@ a =
 10
 print(a)
 a]]
-RUN([[lua -e"_PROMPT='' _PROMPT2=''" -i < %s > %s]], prog, out)
+RUN([[@lua -e"_PROMPT='' _PROMPT2=''" -i < %s > %s]], prog, out)
 checkprogout("6\n10\n10\n\n")
 
 prepfile("a = [[b\nc\nd\ne]]\n=a")
 print("temporary program file: "..prog)
-RUN([[lua -e"_PROMPT='' _PROMPT2=''" -i < %s > %s]], prog, out)
+RUN([[@lua -e"_PROMPT='' _PROMPT2=''" -i < %s > %s]], prog, out)
 checkprogout("b\nc\nd\ne\n\n")
 
 prompt = "alo"
 prepfile[[ --
 a = 2
 ]]
-RUN([[lua "-e_PROMPT='%s'" -i < %s > %s]], prompt, prog, out)
+RUN([[@lua "-e_PROMPT='%s'" -i < %s > %s]], prompt, prog, out)
 local t = getoutput()
 assert(string.find(t, prompt .. ".*" .. prompt .. ".*" .. prompt))
 
@@ -190,8 +199,12 @@ setmetatable(m, {__tostring = function(x)
 end})
 error(m)
 ]]
-NoRun([[lua %s 2> %s]], prog, out)   -- no message
+NoRun([[@lua %s 2> %s]], prog, out)
 checkout(progname..": 6\n")
+
+prepfile("error{}")
+NoRun([[@lua %s 2> %s]], prog, out)
+assert(string.find(getoutput(), "error object is a table value"))
 
 
 s = [=[ -- 
@@ -210,51 +223,51 @@ assert( a == b )
 do return f( 11 ) end  ]=]
 s = string.gsub(s, ' ', '\n\n')
 prepfile(s)
-RUN([[lua -e"_PROMPT='' _PROMPT2=''" -i < %s > %s]], prog, out)
+RUN([[@lua -e"_PROMPT='' _PROMPT2=''" -i < %s > %s]], prog, out)
 checkprogout("101\n13\t22\n\n")
   
 prepfile[[#comment in 1st line without \n at the end]]
-RUN("lua %s", prog)
+RUN("@lua %s", prog)
   
 prepfile[[#test line number when file starts with comment line
 debug = require"debug"
 print(debug.getinfo(1).currentline)
 ]]
-RUN("lua %s > %s", prog, out)
+RUN("@lua %s > %s", prog, out)
 checkprogout('3')
 
 -- close Lua with an open file
 prepfile(string.format([[io.output(%q); io.write('alo')]], out))
-RUN("lua %s", prog)
+RUN("@lua %s", prog)
 checkout('alo')
 
 -- bug in 5.2 beta (extra \0 after version line)
-RUN([[lua -v  -e'print"hello"' > %s]], out)
+RUN([[@lua -v  -e'print"hello"' > %s]], out)
 t = getoutput()
 assert(string.find(t, "PUC%-Rio\nhello"))
 
 
 -- testing os.exit
 prepfile("os.exit(nil, true)")
-RUN("lua %s", prog)
+RUN("@lua %s", prog)
 prepfile("os.exit(0, true)")
-RUN("lua %s", prog)
+RUN("@lua %s", prog)
 prepfile("os.exit(true, true)")
-RUN("lua %s", prog)
+RUN("@lua %s", prog)
 prepfile("os.exit(1, true)")
-NoRun("lua %s", prog)   -- no message
+NoRun("@lua %s", prog)   -- no message
 prepfile("os.exit(false, true)")
-NoRun("lua %s", prog)   -- no message
+NoRun("@lua %s", prog)   -- no message
 
 assert(os.remove(prog))
 assert(os.remove(otherprog))
 assert(not os.remove(out))
 
-RUN("lua -v")
+RUN("@lua -v")
 
-NoRunMsg("lua -h")
-NoRunMsg("lua -e")
-NoRunMsg("lua -e a")
-NoRunMsg("lua -l")
+NoRunMsg("@lua -h")
+NoRunMsg("@lua -e")
+NoRunMsg("@lua -e a")
+NoRunMsg("@lua -l")
 
 print("OK")

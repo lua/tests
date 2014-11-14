e366452c0564773c8c57cdc1943d4beb8dbcfe8a
Roberto Ierusalimschy <roberto@inf.puc-rio.br>
2014-11-14 14:05:25 -0200

    janatorial work + a few more tests (invalid options and double cntrl-C)

--- main.lua
+++ main.lua
@@ -3,6 +3,10 @@
 -- most (all?) tests here assume a reasonable "Unix-like" shell
 if _port then return end
 
+-- use only "double quotes" inside shell scripts (better change to
+-- run on Windows)
+
+
 print ("testing stand-alone interpreter")
 
 assert(os.execute())   -- machine has a system command
@@ -48,65 +52,91 @@ local function checkout (s)
   return t
 end
 
-local function auxrun (...)
-  local s = string.format(...)
-  s = string.gsub(s, "@lua", '"'..progname..'"', 1)
-  return os.execute(s)
+
+local function RUN (p, ...)
+  p = string.gsub(p, "lua", '"'..progname..'"', 1)
+  local s = string.format(p, ...)
+  assert(os.execute(s))
 end
 
-local function RUN (...)
-  assert(auxrun(...))
+local function NoRun (msg, p, ...)
+  p = string.gsub(p, "lua", '"'..progname..'"', 1)
+  local s = string.format(p, ...)
+  s = string.format("%s 2> %s", s, out)  -- will send error to 'out'
+  assert(not os.execute(s))
+  assert(string.find(getoutput(), msg, 1, true))  -- check error message
 end
 
-local function NoRun (...)
-  assert(not auxrun(...))
-end
+RUN('lua -v')
 
-local function NoRunMsg (...)
-  print("\n(the next error is expected by the test)")
-  return NoRun(...)
-end
-
-RUN("@lua -v")
+print(string.format("(temporary program file used in these tests: %s)", prog))
 
 -- running stdin as a file
-RUN("@lua > %s << eof\nprint(10)\nprint(2)\neof\n", out)
+prepfile""
+RUN('lua - < %s > %s', prog, out)
+checkout("")
+
+prepfile[[
+  print(
+1, a
+)
+]]
+RUN('lua - < %s > %s', prog, out)
+checkout("1\tnil\n")
+
+RUN('echo "print(10)\nprint(2)\n" | lua > %s', out)
 checkout("10\n2\n")
 
+
+-- test option '-'
+RUN('echo "print(arg[1])" | lua - -h > %s', out)
+checkout("-h\n")
+
 -- test environment variables used by Lua
+
 prepfile("print(package.path)")
 
-RUN("env LUA_INIT= LUA_PATH=x @lua %s > %s", prog, out)
+-- test LUA_PATH
+RUN('env LUA_INIT= LUA_PATH=x lua %s > %s', prog, out)
 checkout("x\n")
 
-RUN("env LUA_INIT= LUA_PATH_5_3=y LUA_PATH=x @lua %s > %s", prog, out)
+-- test LUA_PATH_version
+RUN('env LUA_INIT= LUA_PATH_5_3=y LUA_PATH=x lua %s > %s', prog, out)
 checkout("y\n")
 
+-- test LUA_CPATH
 prepfile("print(package.cpath)")
-RUN("env LUA_INIT= LUA_CPATH=xuxu @lua %s > %s", prog, out)
+RUN('env LUA_INIT= LUA_CPATH=xuxu lua %s > %s', prog, out)
 checkout("xuxu\n")
 
-RUN("env LUA_INIT= LUA_CPATH_5_3=yacc LUA_CPATH=x @lua %s > %s", prog, out)
+-- test LUA_CPATH_version
+RUN('env LUA_INIT= LUA_CPATH_5_3=yacc LUA_CPATH=x lua %s > %s', prog, out)
 checkout("yacc\n")
 
+-- test LUA_INIT (and its access to 'arg' table)
 prepfile("print(X)")
-RUN('env LUA_INIT="X=tonumber(arg[1])" @lua %s 3.2 > %s', prog, out)
+RUN('env LUA_INIT="X=tonumber(arg[1])" lua %s 3.2 > %s', prog, out)
 checkout("3.2\n")
 
+-- test LUA_INIT_version
 prepfile("print(X)")
-RUN('env LUA_INIT_5_3="X=10" LUA_INIT="X=3" @lua %s > %s', prog, out)
+RUN('env LUA_INIT_5_3="X=10" LUA_INIT="X=3" lua %s > %s', prog, out)
 checkout("10\n")
 
+-- test LUA_INIT for files
 prepfile("x = x or 10; print(x); x = x + 1")
-RUN("env LUA_INIT='@%s' @lua %s > %s", prog, prog, out)
+RUN('env LUA_INIT="@%s" lua %s > %s', prog, prog, out)
 checkout("10\n11\n")
+
+-- test errors in LUA_INIT
+NoRun('LUA_INIT:1: 10', 'env LUA_INIT="error(10)" lua')
 
 -- test option '-E'
 local defaultpath, defaultCpath
 
 do
   prepfile("print(package.path, package.cpath)")
-  RUN('env LUA_INIT="error(10)" LUA_PATH=xxx LUA_CPATH=xxx @lua -E %s > %s',
+  RUN('env LUA_INIT="error(10)" LUA_PATH=xxx LUA_CPATH=xxx lua -E %s > %s',
        prog, out)
   local out = getoutput()
   defaultpath = string.match(out, "^(.-)\t")
@@ -123,7 +153,7 @@ assert(not string.find(defaultpath, "xxx") and
 -- test replacement of ';;' to default path
 local function convert (p)
   prepfile("print(package.path)")
-  RUN('env LUA_PATH="%s" @lua %s > %s', p, prog, out)
+  RUN('env LUA_PATH="%s" lua %s > %s', p, prog, out)
   local expected = getoutput()
   expected = string.sub(expected, 1, -2)   -- cut final end of line
   assert(string.gsub(p, ";;", ";"..defaultpath..";") == expected)
@@ -137,12 +167,13 @@ convert(";;;;;")
 convert(";;a;;;bc")
 
 
--- test 2 files
+-- test -l over multiple libraries
 prepfile("print(1); a=2; return {x=15}")
 prepfile(("print(a); print(_G['%s'].x)"):format(prog), otherprog)
-RUN('env LUA_PATH="?;;" @lua -l %s -l%s -lstring -l io %s > %s', prog, otherprog, otherprog, out)
+RUN('env LUA_PATH="?;;" lua -l %s -l%s -lstring -l io %s > %s', prog, otherprog, otherprog, out)
 checkout("1\n2\n15\n2\n15\n")
 
+-- test 'arg' table
 local a = [[
   assert(#arg == 3 and arg[1] == 'a' and
          arg[2] == 'b' and arg[3] == 'c')
@@ -153,51 +184,45 @@ local a = [[
 ]]
 a = string.format(a, progname)
 prepfile(a)
-RUN('@lua "-e " -- %s a b c', prog)
+RUN('lua "-e " -- %s a b c', prog)   -- "-e " runs an empty command
 
+-- test 'arg' availability in libraries
 prepfile"assert(arg)"
 prepfile("assert(arg)", otherprog)
-RUN('env LUA_PATH="?;;" @lua -l%s - < %s', prog, otherprog)
+RUN('env LUA_PATH="?;;" lua -l%s - < %s', prog, otherprog)
 
-prepfile""
-RUN("@lua - < %s > %s", prog, out)
-checkout("")
+-- test messing up the 'arg' table
+RUN('echo "print(...)" | lua -e "arg[1] = 100" - > %s', out)
+checkout("100\n")
+NoRun("'arg' is not a table", 'echo "" | lua -e "arg = 1" -')
 
 -- test many arguments
 prepfile[[print(({...})[30])]]
-RUN("@lua %s %s > %s", prog, string.rep(" a", 30), out)
+RUN('lua %s %s > %s', prog, string.rep(" a", 30), out)
 checkout("a\n")
 
-RUN([[@lua "-eprint(1)" -ea=3 -e "print(a)" > %s]], out)
+RUN([[lua "-eprint(1)" -ea=3 -e "print(a)" > %s]], out)
 checkout("1\n3\n")
 
-prepfile[[
-  print(
-1, a
-)
-]]
-RUN("@lua - < %s > %s", prog, out)
-checkout("1\tnil\n")
-
+-- test iteractive mode
 prepfile[[
 (6*2-6) -- ===
 a =
 10
 print(a)
 a]]
-RUN([[@lua -e"_PROMPT='' _PROMPT2=''" -i < %s > %s]], prog, out)
+RUN([[lua -e"_PROMPT='' _PROMPT2=''" -i < %s > %s]], prog, out)
 checkprogout("6\n10\n10\n\n")
 
 prepfile("a = [[b\nc\nd\ne]]\n=a")
-print("temporary program file: "..prog)
-RUN([[@lua -e"_PROMPT='' _PROMPT2=''" -i < %s > %s]], prog, out)
+RUN([[lua -e"_PROMPT='' _PROMPT2=''" -i < %s > %s]], prog, out)
 checkprogout("b\nc\nd\ne\n\n")
 
 prompt = "alo"
 prepfile[[ --
 a = 2
 ]]
-RUN([[@lua "-e_PROMPT='%s'" -i < %s > %s]], prompt, prog, out)
+RUN([[lua "-e_PROMPT='%s'" -i < %s > %s]], prompt, prog, out)
 local t = getoutput()
 assert(string.find(t, prompt .. ".*" .. prompt .. ".*" .. prompt))
 
@@ -210,14 +235,13 @@ setmetatable(m, {__tostring = function(x)
 end})
 error(m)
 ]]
-NoRun([[@lua %s 2> %s]], prog, out)
-checkout(progname..": 6\n")
+NoRun(progname .. ": 6\n", [[lua %s]], prog)
 
 prepfile("error{}")
-NoRun([[@lua %s 2> %s]], prog, out)
-assert(string.find(getoutput(), "error object is a table value"))
+NoRun("error object is a table value", [[lua %s]], prog)
 
 
+-- chunk broken in many lines
 s = [=[ -- 
 function f ( x ) 
   local a = [[
@@ -232,72 +256,98 @@ end
 return( f( 100 ) )
 assert( a == b )
 do return f( 11 ) end  ]=]
-s = string.gsub(s, ' ', '\n\n')
+s = string.gsub(s, ' ', '\n\n')   -- change all spaces for newlines
 prepfile(s)
-RUN([[@lua -e"_PROMPT='' _PROMPT2=''" -i < %s > %s]], prog, out)
+RUN([[lua -e"_PROMPT='' _PROMPT2=''" -i < %s > %s]], prog, out)
 checkprogout("101\n13\t22\n\n")
   
 prepfile[[#comment in 1st line without \n at the end]]
-RUN("@lua %s", prog)
+RUN('lua %s', prog)
   
 prepfile[[#test line number when file starts with comment line
 debug = require"debug"
 print(debug.getinfo(1).currentline)
 ]]
-RUN("@lua %s > %s", prog, out)
+RUN('lua %s > %s', prog, out)
 checkprogout('3')
 
 -- close Lua with an open file
 prepfile(string.format([[io.output(%q); io.write('alo')]], out))
-RUN("@lua %s", prog)
+RUN('lua %s', prog)
 checkout('alo')
 
 -- bug in 5.2 beta (extra \0 after version line)
-RUN([[@lua -v  -e'print"hello"' > %s]], out)
+RUN([[lua -v  -e"print'hello'" > %s]], out)
 t = getoutput()
 assert(string.find(t, "PUC%-Rio\nhello"))
 
 
 -- testing os.exit
 prepfile("os.exit(nil, true)")
-RUN("@lua %s", prog)
+RUN('lua %s', prog)
 prepfile("os.exit(0, true)")
-RUN("@lua %s", prog)
+RUN('lua %s', prog)
 prepfile("os.exit(true, true)")
-RUN("@lua %s", prog)
+RUN('lua %s', prog)
 prepfile("os.exit(1, true)")
-NoRun("@lua %s", prog)   -- no message
+NoRun("", "lua %s", prog)   -- no message
 prepfile("os.exit(false, true)")
-NoRun("@lua %s", prog)   -- no message
+NoRun("", "lua %s", prog)   -- no message
 
+-- remove temporary files
 assert(os.remove(prog))
 assert(os.remove(otherprog))
 assert(not os.remove(out))
 
-print('testing Ctrl C')
-do
-  -- Lua script that runs protected infinite loop and then prints '42'
-  local luaprg = 'pcall(function () while true do end end); print(42)'
+-- invalid options
+NoRun("unrecognized option '-h'", "lua -h")
+NoRun("unrecognized option '---'", "lua ---")
+NoRun("unrecognized option '-Ex'", "lua -Ex")
+NoRun("unrecognized option '-vv'", "lua -vv")
+NoRun("unrecognized option '-iv'", "lua -iv")
+NoRun("'-e' needs argument", "lua -e")
+NoRun("syntax error", "lua -e a")
+NoRun("'-l' needs argument", "lua -l")
 
-  -- shell script to run 'luaprg' in background and echo its pid
-  local shellprg = string.format("%s -e '%s' & echo $!", progname, luaprg)
 
-  local f = io.popen(shellprg, "r")   -- run shell script
-  local pid = f:read()   -- get pid for Lua script
-  print("(if this test fails, it may leave an infinite Lua script [pid "
-          .. pid .. "] running in your system)")
-  -- waits a little, so script can reach the pcall loop
-  assert(os.execute("sleep 1"))
-  -- send INT signal to Lua script
-  assert(os.execute(string.format("kill -INT %d", pid)))
-  assert(f:read() == "42")  -- expected output
-  assert(f:close())
+if T then   -- auxiliary library?
+  print("testing 'not enough memory' to create a state")
+  NoRun("not enough memory", "env MEMLIMIT=100 lua")
 end
 print('+')
 
-NoRunMsg("@lua -h")
-NoRunMsg("@lua -e")
-NoRunMsg("@lua -e a")
-NoRunMsg("@lua -l")
+print('testing Ctrl C')
+do
+  -- function to run a script in background, returning its output file
+  -- descriptor and its pid
+  local function runback (luaprg)
+    -- shell script to run 'luaprg' in background and echo its pid
+    local shellprg = string.format('%s -e "%s" & echo $!', progname, luaprg)
+    local f = io.popen(shellprg, "r")   -- run shell script
+    local pid = f:read()   -- get pid for Lua script
+    print("(if this test fails, it may leave an infinite Lua script [pid "
+            .. pid .. "] running in your system)")
+    -- waits a little, so script can start properly
+    assert(os.execute("sleep 1"))
+    return f, pid
+  end
+
+  -- Lua script that runs protected infinite loop and then prints '42'
+  local f, pid = runback"pcall(function () while true do end end); print(42)"
+  -- send INT signal to Lua script
+  assert(os.execute(string.format('kill -INT %d', pid)))
+  assert(f:read() == "42")  -- expected output
+  assert(f:close())
+  print("done")
+
+  -- Lua script in a looooog unbreakable search
+  local f, pid = runback"string.find(string.rep('a', 1000), '.*.*.*.*.*b')"
+  -- must send two INT signals to stop this Lua script
+  assert(os.execute(string.format('kill -INT %d', pid)))
+  assert(os.execute(string.format('kill -INT %d', pid)))
+  assert(f:close())
+  print("done")
+
+end
 
 print("OK")
